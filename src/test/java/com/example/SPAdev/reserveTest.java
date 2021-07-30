7bf34b2f0388ae517f7ff08e6cf03069914a35fe
ChangHee Lee <chbe5082@naver.com>
2021-07-30 22:50:01 +0900

    202107302249 push

--- src/test/java/com/example/SPAdev/reserveTest.java
+++ src/test/java/com/example/SPAdev/reserveTest.java
@@ -13,52 +13,6 @@ import java.time.LocalDate;
 @SpringBootTest
 public class reserveTest {
 
-    @Autowired
-    ReserveService reserveService;
-
-    @Autowired
-    ReserveRepository reserveRepository;
-
-    @Test
-    public void save() throws Exception {
-        ReserveDTO reserveDTO = ReserveDTO.builder()
-                .name("asd1255")
-                .phoneNumber("12300005555")
-                .build();
-        reserveService.save(reserveDTO);
-    }
-
-    @Test
-    public void findReserveByName() throws Exception {
-        String name = "test123";
-
-        reserveService.findReserveByName(name);
-    }
-
-    @Test
-    public void findReserveById() throws Exception {
-        String phoneNumber = "0000";
-
-        reserveService.findReserveByPhoneNumber(phoneNumber);
-    }
-
-    //망함
-    @Test
-    public void modifyReserve() throws Exception{
-        String str = "6";
-        Long id = Long.parseLong(str);
-
-        ReserveDTO reserveDTO = reserveService.getReserveById(id);
-        System.out.println("ReserveDTO 값 : " + reserveDTO);
-        reserveDTO.setPhoneNumber("1234");
-        reserveDTO.setName("홍길동");
-
-
-        reserveService.modifyReserve(reserveDTO);
-    }
-
-
-
 
 
 
