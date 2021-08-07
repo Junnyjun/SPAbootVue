7a0af89d282e0aea92756f7fcc2490c8c1454f3c
ChangHee Lee <chbe5082@naver.com>
2021-08-08 00:45:44 +0900

    202108080024 travis

--- src/test/java/com/example/SPAdev/reserveTest.java
+++ src/test/java/com/example/SPAdev/reserveTest.java
@@ -4,11 +4,7 @@ import com.example.SPAdev.reserve.Reserve;
 import com.example.SPAdev.reserve.ReserveDTO;
 import com.example.SPAdev.reserve.ReserveRepository;
 import com.example.SPAdev.reserve.ReserveService;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.time.LocalDate;
 
 @SpringBootTest
 public class reserveTest {
