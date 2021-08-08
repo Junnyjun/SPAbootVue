2ab02dd337483c4eae7d4d5f87a23a13980ed6ad
ChangHee Lee <chbe5082@naver.com>
2021-08-08 19:24:45 +0900

    202108080024 travis

--- src/main/java/com/example/SPAdev/SpAdevApplication.java
+++ src/main/java/com/example/SPAdev/SpAdevApplication.java
@@ -2,11 +2,7 @@ package com.example.SPAdev;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@EnableJpaAuditing
 @SpringBootApplication
 public class SpAdevApplication{
 
