f681494298f3f0b58114986914e80ef504522973
ChangHee Lee <chbe5082@naver.com>
2021-08-09 23:23:08 +0900

    202108080024 travis

--- src/main/java/com/example/SPAdev/SpAdevApplication.java
+++ src/main/java/com/example/SPAdev/SpAdevApplication.java
@@ -2,20 +2,13 @@ package com.example.SPAdev;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
 public class SpAdevApplication{
 
-	public static final String APPLICATION_LOCATIONS = "spring.config.location="
-			+ "classpath:application.yml,"
-			+ "/app/config/springboot-webservice/real-application.yml";
-
 	public static void main(String[] args) {
-		new SpringApplicationBuilder(SpAdevApplication.class)
-				.properties(APPLICATION_LOCATIONS)
-				.run(args);
+		SpringApplication.run(SpAdevApplication.class, args);
 	}
 }
