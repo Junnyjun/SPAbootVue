bff6b874f0c0df68c92cf5aa4baac2809a0c0d5f
ChangHee Lee <chbe5082@naver.com>
2021-08-09 19:38:27 +0900

    202108080024 travis

--- execute-deploy.sh
+++ execute-deploy.sh
@@ -1,3 +1,3 @@
 echo "execute-deploy exe"
 #!/bin/bash
-/home/ec2-user/app/travis/deploy.sh > /dev/null 2> /dev/null < /dev/null &
+/home/ec2-user/app/nonstop/deploy.sh > /dev/null 2> /dev/null < /dev/null &
